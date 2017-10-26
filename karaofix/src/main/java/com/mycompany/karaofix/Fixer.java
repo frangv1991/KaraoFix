f22b2956d5a7899c97125d57b78ea7321934c4a1
TIC <TIC@PCFORMACION016.ayesa.es>
2017-10-26 20:53:30 +0200

    Commit inicial

--- karaofix/src/main/java/com/mycompany/karaofix/Fixer.java
+++ karaofix/src/main/java/com/mycompany/karaofix/Fixer.java
@@ -5,12 +5,44 @@
  */
 package com.mycompany.karaofix;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author japar
  */
 public class Fixer {
     public static void main(String[] args) {
-        // TODO: Completar el Ejercicio aquí. :-D
+    	
+    	Path documentos = Paths.get("C:\\Users\\TIC\\git\\KaraoFix\\karaofix\\src\\main\\resources\\temazos");
+    	Path copia = Paths.get("C:\\Users\\TIC\\git\\KaraoFix\\karaofix\\src\\main\\resources\\grandesexitos");
+    	
+    	List<Path> carpetas = new ArrayList<Path>();
+    	
+		try {
+			carpetas = Files.list(documentos).collect(Collectors.toList());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	for(Path carpeta:carpetas) {
+    		String path = carpeta.toString();
+    		path = path.replace("temazos", "grandesexitos");
+    		try {
+				Files.createDirectories(Paths.get(path));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}
+    	
     }
 }
